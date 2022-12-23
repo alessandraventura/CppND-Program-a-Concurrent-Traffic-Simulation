662c93d62d0c442e072bfa8496ec622553807b01
Alessandra <alessandra.ventura@dotocean.eu>
2022-12-23 16:57:20 +0100

    defined TrafficLight class

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -31,20 +31,29 @@ class MessageQueue {
 // the private member _currentPhase which can take „red“ or „green“ as its
 // value.
 
-class TrafficLight {
+enum class TrafficLightPhase { red, green };
+
+class TrafficLight : public TrafficObject {
  public:
   // constructor / desctructor
 
   // getters / setters
 
   // typical behaviour methods
+  void WaitForGreen();
+  void Simulate();
+
+  TrafficLightPhase CurrentPhase();
 
  private:
   // typical behaviour methods
+  void CycleThroughPhases();
 
-  // FP.4b : create a private member of type MessageQueue for messages of type
-  // TrafficLightPhase and use it within the infinite loop to push each new
-  // TrafficLightPhase into it by calling send in conjunction with move
+  TrafficLightPhase _current_phase = TrafficLightPhase::red;
+
+  // FP.4b : create a private member of type MessageQueue for messages of
+  // type TrafficLightPhase and use it within the infinite loop to push each
+  // new TrafficLightPhase into it by calling send in conjunction with move
   // semantics.
 
   std::condition_variable _condition;
